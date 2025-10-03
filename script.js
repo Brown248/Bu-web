0b0192a592e7b73df54fe757cbeae161ce0a52e9
Brown248 <Suwat.You248@gmail.com>
2025-10-03 12:53:55 +0700

    Add assignment1 files

---
+++ script.js
@@ -0,0 +1,9 @@
+let input = prompt("Input integer number");
+
+let number = parseInt(input);
+
+if (number === 0) {
+    alert("zero integer number");} 
+    else if (number % 2 === 0) {
+    alert("even integer number");} 
+    else {alert("odd integer number");}
